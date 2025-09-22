fa4a0d27fddf7f9aa608217721879e9b8b261ff0
AndersonSantosAraujo <anderson.dos.santos.araujo@hotmail.com>
2025-09-22 00:16:49 -0300

    feat: test image picture

--- client/src/components/Image/index.tsx
+++ client/src/components/Image/index.tsx
@@ -1,27 +1,36 @@
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import type { ImageProps } from "@/types/image";
 import placeholder from "/placeholder.png";
 
-const Image = ({ url, loading = undefined, fetchpriority = "high", alt = "", title }: ImageProps) => {
+const Image = ({
+  url,
+  loading = "lazy",
+  fetchpriority = "high",
+  alt = "",
+  title,
+}: ImageProps) => {
   const cloudinary = import.meta.env.VITE_CLOUDINARY_URL;
 
   // Padrões de imagem via Cloudinary
-  const small = `${cloudinary}f_auto/q_auto/c_scale,w_480/${url}`;
-  const medium = `${cloudinary}f_auto/q_auto/c_scale,w_991/${url}`;
-  const large = `${cloudinary}f_auto/q_auto/c_scale,w_1280/${url}`;
+  const small = `${cloudinary}f_auto/q_auto/c_scale,w_480${url}`;
+  const medium = `${cloudinary}f_auto/q_auto/c_scale,w_991${url}`;
+  const large = `${cloudinary}f_auto/q_auto/c_scale,w_1280${url}`;
 
   return (
-    <LazyLoadImage
-      src={small}
-      srcSet={`${small} 480w, ${medium} 991w, ${large} 1280w`}
-      sizes="(max-width: 480px) 100vw, (max-width: 991px) 100vw, 1280px"
-      placeholderSrc={placeholder}
-      effect="blur"
-      fetchPriority={fetchpriority} 
-      loading={loading}
-      alt={alt}
-      title={title || alt}
-    />
+    <picture>
+      <source media="(min-width: 992px)" srcSet={large} />
+      <source media="(min-width: 481px)" srcSet={medium} />
+      <img
+        src={small}
+        loading={loading}
+        fetchPriority={fetchpriority}
+        alt={alt}
+        title={title || alt}
+        style={{ width: "100%", height: "auto" }}
+        onError={(e) => {
+          (e.currentTarget as HTMLImageElement).src = placeholder;
+        }}
+      />
+    </picture>
   );
 };
 
